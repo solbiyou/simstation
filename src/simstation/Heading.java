20200cb17e5bcddda09d11912321c50b93a0b8c7
Solbi You <syou9691@gmail.com>
2020-04-08 22:51:26 -0700

    Add random method

--- src/simstation/Heading.java
+++ src/simstation/Heading.java
@@ -1,9 +1,32 @@
 /**
  * Edit History
  * 4/6 edited by Solbi You
+ * 4/8 edited by Solbi You "Add random method"
  */
 package simstation;
 
+import mvc.Utilities;
+
 public enum Heading {
 	NORTH, EAST, SOUTH, WEST;
+	
+	//generating random Heading
+	public static Heading random() {
+		int random = Utilities.rng.nextInt(4);
+		if(random == 0) {
+			return NORTH;
+		}
+		else if(random == 1) {
+			return EAST;
+		}
+		else if(random == 2) {
+			return SOUTH;
+		}
+		else if(random == 3) {
+			return WEST;
+		}
+		else {
+			return null;
+		}
+	}
 }
