fa9dd4e39072b5d5af405cb7a84785c2e1bd9ded
Solbi You <syou9691@gmail.com>
2020-04-09 00:17:28 -0700

    Add command methods

--- src/simstation/Simulation.java
+++ src/simstation/Simulation.java
@@ -1,6 +1,8 @@
 /**
  * Edit History
- * 4/7 edited by Solbi You
+ * 4/7 Solbi You: file created
+ * 4/8 Solbi You: add the methods for timer
+ * 4/8 Solbi You: add command methods
  */
 package simstation;
 
@@ -9,7 +11,7 @@ import java.util.*;
 
 public class Simulation extends Model {
 	public static int SIZE = 250;
-	protected List<Agent> agents;
+	private List<Agent> agents;
 	private int clock;
 	private Timer timer;
 	
@@ -22,6 +24,34 @@ public class Simulation extends Model {
 		agents.add(a);
 	}
 	
+	//command methods
+	public void start() {
+		startTimer();
+		for(int i=0; i < agents.size(); i++) {
+			agents.get(i).start();
+		}
+	}
+	
+	public void suspend() {
+		stopTimer();
+		for(int i=0; i < agents.size(); i++) {
+			agents.get(i).suspend();
+		}
+	}
+	
+	public void resume() {
+		resumeTimer();
+		for(int i=0; i < agents.size(); i++) {
+			agents.get(i).resume();
+		}
+	}
+	
+	public void stop() {
+		for(int i=0; i < agents.size(); i++) {
+			agents.get(i).stop();
+		}
+	}
+	
 	//empty method that will be specified in subclasses
 	public void populate() {
 		
@@ -30,6 +60,10 @@ public class Simulation extends Model {
 	//methods for timer
 	private void startTimer() {
 		timer = new Timer();
+		timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+	}
+	
+	private void resumeTimer() {
 		timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
 	}
 
