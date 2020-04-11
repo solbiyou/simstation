0bf4bf1bd8a028e7a1418f98930ce5bcde6e98e7
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-04-11 14:14:05 -0700

    Update SimulationView.java

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -1,9 +1,35 @@
 /**
  * Edit History
- * 
+ * 4/10 Katrina Slivkoff created paint component
+ *  
  */
 package simstation;
 
-public class SimulationView {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
 
-}
+import mvc.*;
+
+public class SimulationView extends View {
+
+	private static final long serialVersionUID = 1L;
+
+	public SimulationView(Simulation simulation) {
+		super(simulation);
+	}
+
+	public void paintComponent(Graphics gc) {
+		Simulation simulation = (Simulation)model;
+		Color oldColor = gc.getColor();
+		gc.setColor(Color.RED);
+    List<Agent> list = simulation.getAgents();
+    
+		for (Agent agent: list) {
+			gc.fillOval(agent.xc, agent.yc, 5, 5);
+		}
+
+		gc.setColor(oldColor);
+	}
+
+}	
