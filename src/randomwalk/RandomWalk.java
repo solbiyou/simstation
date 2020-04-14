17b15b17abd75df3e15ae360b9f359144d9022c1
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-04-13 20:19:14 -0700

    Update RandomWalk.java

--- src/randomwalk/RandomWalk.java
+++ src/randomwalk/RandomWalk.java
@@ -1,9 +1,22 @@
 /**
  * Edit History
+ * 4/13 Katrina Slivkoff implemented the update method with a random heading and move a random num of steps
  * 
  */
 package randomwalk;
 
-public class RandomWalk {
+import mvc.*;
+import simstation.*;
 
+public class RandomWalk extends Agent {
+
+	public RandomWalk() {
+		super("random walk");
+	}
+
+	public void update() {
+		setHeading(Heading.random());
+		move(Utilities.rng.nextInt(6) + 1);
+	}
 }
+
