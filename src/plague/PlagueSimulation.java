dc30e5165f964551be8a47241de2dd2b64fdb8bf
Solbi You <syou9691@gmail.com>
2020-04-14 11:05:24 -0700

    Adjust the number of percentages to demonstrate proper infection rate

--- src/plague/PlagueSimulation.java
+++ src/plague/PlagueSimulation.java
@@ -1,6 +1,7 @@
 /**
  * Edit History
  * 4/13 Solbi You implemented PlagueSimulation with overriding methods
+ * 4/14 Solbi You adjusted number of chances to demonstrate proper infection rate
  */
 package plague;
 
@@ -8,9 +9,9 @@ import mvc.AppPanel;
 import simstation.*;
 
 public class PlagueSimulation extends Simulation {
-	public static final int VIRULENCE = 50;
-	public static final int RESISTANCE = 2;
-	public static int AGENT_NUM = 50;
+	public static int VIRULENCE = 10;		//chance of initial infections of the agents 
+	public static int RESISTANCE = 50;		//chance of resisting infection even if the neighbor tries to infect the agent
+	public static int AGENT_NUM = 50;		//number of agent the simulation producing
 	
 	public void populate() {
 		for(int i = 0; i < AGENT_NUM; i++) {
