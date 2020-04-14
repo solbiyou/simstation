c14f3e91681197e78bbff29536857058ad2ace00
Solbi You <syou9691@gmail.com>
2020-04-13 21:45:14 -0700

    Update file with overriding methods

--- src/plague/PlagueFactory.java
+++ src/plague/PlagueFactory.java
@@ -1,9 +1,21 @@
 /**
  * Edit History
- * 
+ * 4/13 Solbi You implemented PlagueFactory by overriding methods
  */
 package plague;
 
-public class PlagueFactory {
+import mvc.*;
+import simstation.*;
 
+public class PlagueFactory extends SimulationFactory{
+	public Model makeModel() {
+		PlagueSimulation plague = new PlagueSimulation();
+		return plague;
+	}
+	public View getView(Model model) {
+		PlagueView view = new PlagueView((PlagueSimulation) model);
+		return view;
+	}
+	
+	
 }
