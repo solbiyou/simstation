0dad117b3f9eb43a99fe720427aa330abe94025d
Solbi You <syou9691@gmail.com>
2020-04-13 21:47:40 -0700

    Update plagueView for showing two colors

--- src/plague/PlagueView.java
+++ src/plague/PlagueView.java
@@ -1,9 +1,36 @@
 /**
  * Edit History
- * 
+ * 4/13 Solbi You implemented PlagueView to represent two colors for plague
  */
 package plague;
 
-public class PlagueView {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
 
+import simstation.*;
+import mvc.*;
+
+public class PlagueView extends SimulationView {
+	public PlagueView(PlagueSimulation plague) {
+		super(plague);
+	}
+	public void paintComponent(Graphics gc) {
+		PlagueSimulation simulation = (PlagueSimulation)model;
+		Color oldColor = gc.getColor();
+		//gc.setColor(Color.RED);
+		List<Agent> list = simulation.getAgents();
+		for (Agent agent: list) {
+			Plague plague = (Plague)agent;
+			if(plague.isInfected()) {
+				gc.setColor(Color.RED);
+			}
+			else {
+				gc.setColor(Color.GREEN);
+			}
+			gc.fillOval(agent.getXc(), agent.getYc(), 5, 5);
+		}
+
+		gc.setColor(oldColor);
+	}
 }
