ff73127533f715cde43a85f06b97b1746dd58244
Solbi You <syou9691@gmail.com>
2020-04-13 21:46:31 -0700

    Update the file with overriding methods

--- src/plague/PlagueSimulation.java
+++ src/plague/PlagueSimulation.java
@@ -1,9 +1,29 @@
 /**
  * Edit History
- * 
+ * 4/13 Solbi You implemented PlagueSimulation with overriding methods
  */
 package plague;
 
-public class PlagueSimulation {
+import mvc.AppPanel;
+import simstation.*;
 
+public class PlagueSimulation extends Simulation {
+	public static int VIRULENCE = 50;
+	public static int RESISTANCE = 2;
+	
+	public void populate() {
+		for(int i = 0; i < 50; i++) {
+			addAgent(new Plague());
+		}
+	}
+	
+	public String getStats() {
+		String infectiontStat = super.getStats() + "\n%infected: " + ((Plague) getAgents()).getInfectedPct() ; 
+		return infectiontStat;
+	}
+	
+	public static void main(String[] args) {
+		AppPanel panel = new SimulationPanel(new PlagueFactory());
+		panel.display();
+	}
 }
