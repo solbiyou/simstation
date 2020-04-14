e6b6ce8d74cb0a70a921c4f37e6057411154b4a2
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-04-13 20:20:10 -0700

    Update RandomWalkSimulation.java

--- src/randomwalk/RandomWalkSimulation.java
+++ src/randomwalk/RandomWalkSimulation.java
@@ -1,9 +1,22 @@
 /**
  * Edit History
- * 
+ * 4/13 Katrina Slivkoff implemented the populate and main method
  */
 package randomwalk;
 
-public class RandomWalkSimulation {
+import mvc.*;
+import simstation.*;
 
+public class RandomWalkSimulation extends Simulation{
+
+	public void populate() {
+		for(int i = 0; i < 50; i++) {
+			addAgent(new RandomWalk());
+		}
+	}
+	
+	public static void main(String[] args) {
+		AppPanel panel = new SimulationPanel(new RandomWalkFactory());
+		panel.display();
+	}
 }
