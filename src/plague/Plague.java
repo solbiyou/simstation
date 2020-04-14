3f68c02d0ed81b3f3be99566e125c118314fa09b
Solbi You <syou9691@gmail.com>
2020-04-13 21:44:07 -0700

    Update the file without update method

--- src/plague/Plague.java
+++ src/plague/Plague.java
@@ -1,9 +1,45 @@
 /**
  * Edit History
- * 
+ * 4/13 Solbi You implemented Plague class. update method not finished yet
  */
 package plague;
 
-public class Plague {
+import boid.Boid;
+import mvc.Utilities;
+import simstation.*;
+
+public class Plague extends Agent {
+	private boolean isInfected;
+	private double infectedPct;
+	private int steps;
+	
+	public Plague() {
+		super("plague");
+		isInfected = false;
+		steps = Utilities.rng.nextInt(6) + 1;
+	}
+
+	@Override
+	public void update() {
+		Plague neighbor = (Plague)this.getWorld().getNeighbor(this, 15);
+		if (neighbor != null) {
+			
+		}
+		setHeading(Heading.random());
+		move(steps);
+	}
+	
+	//getters
+	public boolean isInfected() {
+		return isInfected;
+	}
+
+	public double getInfectedPct() {
+		return infectedPct;
+	}
+
+	public int getSteps() {
+		return steps;
+	}
 
 }
