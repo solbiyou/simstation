dc657f82aeeb0d6bd270726929226f8a8db4fc07
Solbi You <syou9691@gmail.com>
2020-04-15 12:27:11 -0700

    Change the logic according to the professor's

--- src/plague/Plague.java
+++ src/plague/Plague.java
@@ -3,6 +3,7 @@
  * 4/13 Solbi You implemented Plague class. update method not finished yet
  * 4/13 Solbi You implemented update method
  * 4/14 Solbi You changed the logic in update method
+ * 4/15 Solbi You changed the logic according to the professor's
  */
 package plague;
 
@@ -12,14 +13,16 @@ import simstation.*;
 public class Plague extends Agent {
 	private boolean isInfected;
 	public static double infectedPct = 0;
+	public static int initialInfectionNUm = 0;
 	private int steps;
 	
 	public Plague() {
 		super("plague");
-		int randPctNum = Utilities.rng.nextInt(100) + 1;
-		if(randPctNum < PlagueSimulation.VIRULENCE) {
+		//initial infection for the first INITIAL_NUM of agents
+		if(Plague.initialInfectionNUm < PlagueSimulation.INITIAL_NUM) {
 			isInfected = true;
 			synchronized(this) {
+				Plague.initialInfectionNUm++;		
 				Plague.infectedPct += (double)100/PlagueSimulation.AGENT_NUM;	//increase the percentage when the agent gets infected
 			}
 		}
@@ -35,7 +38,7 @@ public class Plague extends Agent {
 		int randPctNum = Utilities.rng.nextInt(100) + 1;
 		if (neighbor != null) {
 			// if the neighbor is infected and this agent is not yet infected then this agent gets infected unless it resists
-			if(!isInfected && neighbor.isInfected && randPctNum > PlagueSimulation.RESISTANCE) {
+			if(!isInfected && neighbor.isInfected && randPctNum > PlagueSimulation.RESISTANCE && randPctNum < PlagueSimulation.VIRULENCE) {
 				synchronized(this) {
 					Plague.infectedPct += (double)100/PlagueSimulation.AGENT_NUM;	//increase the percentage when the agent gets infected
 					isInfected = true;
