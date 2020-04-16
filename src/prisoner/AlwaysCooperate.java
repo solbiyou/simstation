4ce4a3618348682f5bd4410255587a9573571308
Katrina Slivkoff <55104755+katrinaslivkoff@users.noreply.github.com>
2020-04-15 20:47:33 -0700

    Update AlwaysCooperate.java

--- src/prisoner/AlwaysCooperate.java
+++ src/prisoner/AlwaysCooperate.java
@@ -1,3 +1,9 @@
+/**
+ * Edit History 
+ * 4/15 Katrina Slivkoff created the AlwaysCooperate class that implements Strategy
+ * 
+ */
+
 package prisoner;
 
 public class AlwaysCooperate implements Strategy {
